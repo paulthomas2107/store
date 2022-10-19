868c987eefe03e7d86f5fc7c890b43d900e6826a
Paul Thomas <paulthomas2107@gmail.com>
2022-10-19 14:10:01 +0100

    Products....

---
+++ src/productsStore.js
@@ -0,0 +1,20 @@
+
+const productsArray = [
+    {
+        id: "1",
+        title: "Coffee",
+        price: 4.99
+    },
+    {
+        id: "2",
+        title: "Sunglasses",
+        price: 9.99
+    },
+    {
+        id: "3",
+        title: "Camera",
+        price: 39.99
+    }
+]
+
+export {productsArray};
