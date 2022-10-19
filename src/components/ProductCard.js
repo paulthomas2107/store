9acd4a5dc7f857d9cfda14b963aa7ae71577e27e
Paul Thomas <paulthomas2107@gmail.com>
2022-10-19 16:39:28 +0100

    Add button function

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -20,10 +20,11 @@ function ProductCard(props) {
                 <Form as={Row}>
                     <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
                     <Col sm="6">
-                        <Button sm="6" className='mx-2'>+</Button>
-                        <Button sm="6" className='mx-2'>-</Button>
+                        <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className='mx-2'>+</Button>
+                        <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className='mx-2'>-</Button>
                     </Col>
                 </Form>
+                <Button variant='danger' onClick={() => cart.deleteFromCart(product.id)} className='my-2'>Remove from cart</Button>
                 </>
                 : 
                 <Button variant='primary' onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
